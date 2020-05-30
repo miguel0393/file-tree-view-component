ac7fe002b2d45eed5a772fe8a18cae9928dcab67
oesoto <oesoto@gmail.com>
2020-05-29 21:09:59 -0500

    Context menu

--- src/components/ContextMenu.vue
+++ src/components/ContextMenu.vue
@@ -1,68 +1,96 @@
 <template>
-    <div id="app" class="custom-cm"
-        v-if="visible"
-        :style="{ 'top': yPosition + 'px', 'left': xPosition + 'px'}" 
-        @contextmenu="(e) => e.preventDefault()"
-    > 
-        <div class="custom-cm__item" @click="changeName">Cambiar Nombre</div>
-        <div class="custom-cm__item" @click="addFolder">Agregar Carpeta</div>
-        <div class="custom-cm__item" @click="addFile">Agregar Archivo</div>
-    </div>
-    
+  <div
+    id="app"
+    class="custom-cm"
+    v-if="visible"
+    :style="{ 'top': yPosition + 'px', 'left': xPosition + 'px'}"
+    @contextmenu="(e) => e.preventDefault()"
+  >
+    <div
+      v-for="(option, index) in currentOptions"
+      class="custom-cm__item"
+      :key="index"
+      @click="handleMenuClick(option.function)"
+    >{{ option.name }}</div>
+
+  </div>
 </template>
 
 <script>
 export default {
-    name: "ContextMenu",
-    props: {
-        visible: {
-            type: Boolean,
-            default: false
-        },
-        xPosition: {
-            type: Number,
-            default: 0
-        },
-        yPosition: {
-            type: Number,
-            default: 0
-        }        
+  name: "ContextMenu",
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
     },
-    methods: {
-        changeName() {
-            this.visible = false;
-            this.$emit("onClick", "Cambiar Nombre", "Ingrese el nombre del elemento");
-        },
-        addFolder() {
-            this.visible = false;
-            this.$emit("onClick", "Agregar Carpeta", "Ingrese el nombre del elemento");
-        },
-        addFile() {
-            this.visible = false;
-            this.$emit("onClick", "Agregar Archivo", "Ingrese el nombre del elemento");
-        }
+    xPosition: {
+      type: Number,
+      default: 0
+    },
+    yPosition: {
+      type: Number,
+      default: 0
+    },
+    menuType: {
+      type: String,
+      default: ""
     }
-}
+  },
+  data() {
+    return {
+      menuOptions: {
+        root: [
+          { name: "Delete tree", function: "delete-tree" },
+          { name: "Add Subfolder", function: "add-subfolder" },
+          { name: "Expand/Collapse", function: "toggle-expand-tree" }
+        ],
+        folder: [
+          { name: "Delete subfolder", function: "delete-subfolder" },
+          { name: "Add file", function: "add-file" },
+          { name: "Expand/collapse", function: "toggle-expand-node" }
+        ],
+        file: [
+          { name: "Delete file", function: "delete-file" },
+          { name: "Modify file", function: "modify-file" },
+          { name: "Show file", function: "show-file" }
+        ]
+      },
+      currentOptions: [],
+    //   visible: false
+    };
+  },
+  methods: {
+    handleMenuClick(option) {
+        this.$emit('onClick', option);
+    },
+  },
+  beforeUpdate() {
+    if (this.menuType !== "") {
+      this.currentOptions = this.menuOptions[this.menuType];
+    }
+  }
+};
 </script>
 
 <style scoped>
-    .custom-cm {
-        text-align: left;
-        color: black;
-        background-color: aliceblue;
-        border: 1px solid #cccccc;
-        box-shadow: 1px 1px 10px rgb(0, 0, 0, 0.1);
-        padding: 10px 0px;
-        position: absolute;
-        width: 200px;
-    }
+.custom-cm {
+  text-align: left;
+  color: black;
+  background-color: aliceblue;
+  border: 1px solid #cccccc;
+  box-shadow: 1px 1px 10px rgb(0, 0, 0, 0.1);
+  padding: 10px 0px;
+  position: absolute;
+  width: 200px;
+}
 
-    .custom-cm__item {
-        cursor: pointer;
-        padding: 8px 15px;
-    }
+.custom-cm__item {
+  cursor: pointer;
+  padding: 8px 15px;
+}
 
-    .custom-cm__item:hover {
-        background-color: antiquewhite;
-    }
+.custom-cm__item:hover {
+  background-color: antiquewhite;
+}
 </style>
